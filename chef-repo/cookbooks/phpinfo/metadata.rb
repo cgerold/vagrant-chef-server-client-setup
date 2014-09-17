b3d18f2627b602fc07fd581a375e5de50e5173c0
Christoph Gerold <c.gerold@gmail.com>
2014-09-17 19:02:54 +0200

    bumped version, added usage of file_path attribute

--- chef-repo/cookbooks/phpinfo/metadata.rb
+++ chef-repo/cookbooks/phpinfo/metadata.rb
@@ -2,6 +2,6 @@ name             'phpinfo'
 maintainer       'Christoph Gerold'
 maintainer_email 'c.gerold@gmail.com'
 license          'All rights reserved'
-description      'Creates a index.php with phpinfo() and puts the file to the default vhost'
+description      'Creates a index.php with phpinfo() and puts the file to the desired location'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.2.0'
--- chef-repo/cookbooks/phpinfo/recipes/default.rb
+++ chef-repo/cookbooks/phpinfo/recipes/default.rb
@@ -7,14 +7,13 @@
 # All rights reserved - Do Not Redistribute
 #
 
-# remove index.html from document root
-
-file '/var/www/index.html' do
-	action :delete
+  # remove index.html from desired path
+file "#{node['phpinfo']['file_path']}index.html" do
+  action :delete
 end
 
-# add index.php to document root
+  # add index.php to desired path
 template "#{node['phpinfo']['file_path']}index.php" do
-	source 'index.php.erb'
-	action :create_if_missing
+  source 'index.php.erb'
+  action :create_if_missing
 end
